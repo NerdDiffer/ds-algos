34294141580d7d3913c72e695dc6a51c7aa812db
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-10 12:24:16 -0700

    Add WeightedQuickUnion. Squashed commit of the following:
    commit 03c4bb723c9bc9c544751c907e1a70cff6559eec
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 10 12:14:57 2015 -0700
    
        Add shared specs for weighted Quick Union
    
    commit 3a8b38d0af7e773bbc65b1b388a5318dc2e4fd62
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 10 12:13:39 2015 -0700
    
        Add a shared behavior spec for weighted quick union
    
        Store a lot of repeated specs in a function
    
    commit 613f4f5cb494037c5bdff83f1777d3ecc0f03253
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 10 12:11:31 2015 -0700
    
        Fix 'sizes' array for weighted QuickUnion
    
        When initializing a weighted QuickUnion, each value of the sizes array
        should be 1. Also, remove a trailing non-breaking space in QuickFind.
    
    commit d1654d4ae1a8aeac560f30a664a9a775c00f778f
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 10 00:55:16 2015 -0700
    
        Add a weighted option for quickUnion data structure

--- spec/lib/quickUnion_spec.js
+++ spec/lib/quickUnion_spec.js
@@ -12,6 +12,18 @@ describe('QuickUnion', function() {
     sharedBehavior.demonstratesInheritance(obj, QuickUnion, QuickFind);
   });
 
+  describe('distinguishing between weighted quick union', function() {
+
+    it('should have `weighted` value of false', function() {
+      assert(!obj.weighted);
+    });
+
+    it('should NOT have a `sizes` property', function() {
+      assert(!obj.hasOwnProperty('sizes'));
+    });
+
+  });
+
   describe('combining methods', function() {
     var anotherObj = new QuickUnion(10);
 
@@ -73,6 +85,136 @@ describe('QuickUnion', function() {
         anotherObj.union(1, 0);
         assert.deepEqual(anotherObj.ids, [1,1,1,8,3,0,5,1,8,8]);
       });
+    });
+  });
+});
+
+describe('weighted QuickUnion', function() {
+
+  var obj = new QuickUnion(5, true);
+
+  describe('two properties of weighted quick union', function() {
+
+    it('should have `weighted` value of true', function() {
+      assert(obj.weighted);
+    });
+
+    it('should have a `sizes` property', function() {
+      assert(obj.hasOwnProperty('sizes'));
+    });
+
+  });
+
+  describe('combining methods', function() {
+
+    var obj = new QuickUnion(10, true);
+
+    describe('states after union(4, 7)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [4, 7],
+        [0,1,2,3,4,5,6,4,8,9],
+        [1,1,1,1,2,1,1,1,1,1],
+        9
+      );
+
+    });
+
+    describe('states after union(2, 0)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [2, 0],
+        [2,1,2,3,4,5,6,4,8,9],
+        [1,1,2,1,2,1,1,1,1,1],
+        8
+      );
+
+    });
+
+    describe('states after union(5, 6)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [5, 6],
+        [2,1,2,3,4,5,5,4,8,9],
+        [1,1,2,1,2,2,1,1,1,1],
+        7
+      );
+
+    });
+
+    describe('states after union(7, 9)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [7, 9],
+        [2,1,2,3,4,5,5,4,8,4],
+        [1,1,2,1,3,2,1,1,1,1],
+        6
+      );
+
+    });
+
+    describe('states after union(8, 3)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [8, 3],
+        [2,1,2,8,4,5,5,4,8,4],
+        [1,1,2,1,3,2,1,1,2,1],
+        5
+      );
+
+    });
+
+    describe('states after union(3, 2)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [3, 2],
+        [2,1,8,8,4,5,5,4,8,4],
+        [1,1,2,1,3,2,1,1,4,1],
+        4
+      );
+
+    });
+
+    describe('states after union(4, 6)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [4, 6],
+        [2,1,8,8,4,4,5,4,8,4],
+        [1,1,2,1,5,2,1,1,4,1],
+        3
+      );
+
+    });
+
+    describe('states after union(8, 4)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [8, 4],
+        [2,1,8,8,4,4,5,4,4,4],
+        [1,1,2,1,9,2,1,1,4,1],
+        2
+      );
+
+    });
+
+    describe('states after union(3, 1)', function() {
+
+      sharedBehavior.theseStatesAfterUnionOp(
+        obj,
+        [3, 1],
+        [2,4,8,8,4,4,5,4,4,4],
+        [1,1,2,1,10,2,1,1,4,1],
+        1
+      );
+
     });
   });
 });
