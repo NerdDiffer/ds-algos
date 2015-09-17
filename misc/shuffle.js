2826caff4e97575dde715299df20aed059b1d8fa
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-17 00:27:00 -0700

    Fix bugs in the 'shuffle' function
    No need to require the helper file...That was a a leftover.
    Also, fix infinite loop (b/c I didn't update reference to new array
    length).

--- misc/shuffle.js
+++ misc/shuffle.js
@@ -2,21 +2,21 @@
  * Knuth, or Fisher-Yates, shuffle
  */
 
-var h = require('./helpers.js');
-
 module.exports = function (arr) {
 
   var shuffled = [];
 
   var n = arr.length;
 
-  while (n > 0) {
+  while (n) {
     // select random value from arr
     var val = arr[generateRandomInt(0, n - 1)];
     // remove the value from arr
     arr.splice(arr.indexOf(val), 1);
     // add the value to shuffled
     shuffled.push(val);
+    // update `n` to new array length or die of an infinite loop
+    n = arr.length;
   }
 
   return shuffled;
