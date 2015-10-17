d533bc6d1a6c502a577a3931dc86baf3a10b7e84
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-10-17 15:04:07 -0700

    Add 2nd implementation of TopDown MergeSort. Squashed commit of the following:
    commit 8c9c55a17b02866e713a57af20584fd3291d140e
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sat Oct 17 13:33:03 2015 -0700
    
        Add another implementation of top-down merge sort
    
        Original implementation is still available.
    
        Require both merging algorithms from same file & further assign merge algorithm
        where appropriate.
    
        Newer implementation uses simpler merging algorithm.
    
        Export both implementations.
    
    commit e9aa9d0edc55d6f190c462c3c1d8a81b4c0a1843
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sat Oct 17 13:25:38 2015 -0700
    
        Update require call & add comment for bottom-up merge sort
    
        Specify the first merging algorithm for this version of bottom-up merge sort.
    
        Add a clarification comment for an internal variable.
    
    commit 77335c0873f10bd9ecf915d7d36ae9597f199730
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sat Oct 17 13:22:49 2015 -0700
    
        Add a simpler merging algorithm
    
        It is within the same file as the first merging algorithm. See comments for the
        algorithm source. The module.exports object is no longer a function, so any
        files requiring any of the two merging functions must specify which of the two.
    
    commit 37d1263b7b0d4e0c2b7f2f4778660ae092c393ab
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sat Oct 17 13:18:46 2015 -0700
    
        Add specs & make room for 2nd implementation of MergeSort-TopDown
    
        All 3 variations will use the same helper methods (for shuffling the test
        array and for asserting it is NOT ordered). So, just require those functions
        one time at the beginning.
    
        Make room for 2nd implementation of Top down merge sort. Add the same specs,
        but adjust require calls where necessary (new implementation is in same file as
        the first one for top down merge sort).
    
        Update the wording in describe blocks.

--- spec/sorting/mergeSort_spec.js
+++ spec/sorting/mergeSort_spec.js
@@ -1,48 +1,70 @@
 var assert = require('assert');
 var sorts = require('../../').sorts;
+
 var commonBehavior = require('./_commonSortingBehavior.js');
+var h = require('../../lib/sorting/helpers.js');
+var shuffle = require('../../lib/misc/shuffle.js');
 
-describe('Merge Sort', function() {
-  var mergeSort = sorts.mergeSort;
-  commonBehavior.sortsBasicCases(mergeSort);
+describe('Merge Sort, Top-Down, Recursive implementations', function() {
 
-  describe('sorting a shuffled array of 100 distinct integers', function() {
-    var shuffle = require('../../lib/misc/shuffle.js');
-    var h = require('../../lib/sorting/helpers.js');
+  context('First implementation', function() {
+    var mergeSort = sorts.mergeSort.mergeSort1;
+    commonBehavior.sortsBasicCases(mergeSort);
 
-    var arr = [];
-    for (var i = 1; i <= 100; i++) { arr.push(i); }
-    var sh = shuffle(arr);
+    describe('sorting a shuffled array of 100 distinct integers', function() {
+      var arr = [];
+      for (var i = 1; i <= 100; i++) { arr.push(i); }
+      var sh = shuffle(arr);
 
+      context('setup', function() {
+        it('confirms the array is NOT sorted', function() {
+          // OK, except for 1 in every `100!` times...
+          assert(!h.sorted(sh));
+        });
+      });
 
-    context('setup', function() {
-      it('confirms the array is NOT sorted', function() {
-        // OK, except for 1 in every `100!` times...
-        assert(!h.sorted(sh));
+      context('verify', function() {
+        it('sorts the array', function() {
+          mergeSort(sh);
+          assert(h.sorted(sh));
+        });
       });
     });
+  });
 
-    context('verify', function() {
-      it('sorts the array', function() {
-        mergeSort(sh);
-        assert(h.sorted(sh));
+  context('Second implementation', function() {
+    var mergeSort = sorts.mergeSort.mergeSort2;
+    commonBehavior.sortsBasicCases(mergeSort);
+
+    describe('sorting a shuffled array of 100 distinct integers', function() {
+      var arr = [];
+      for (var i = 1; i <= 100; i++) { arr.push(i); }
+      var sh = shuffle(arr);
+
+      context('setup', function() {
+        it('confirms the array is NOT sorted', function() {
+          // OK, except for 1 in every `100!` times...
+          assert(!h.sorted(sh));
+        });
+      });
+
+      context('verify', function() {
+        it('sorts the array', function() {
+          assert(mergeSort(sh));
+        });
       });
     });
   });
 });
 
-describe('Merge Sort, Bottom-Up', function() {
+describe('Merge Sort, Bottom-Up, Iterative implementation', function() {
   var mergeSort = sorts.mergeSort_bottomUp;
   commonBehavior.sortsBasicCases(mergeSort);
 
   describe('sorting a shuffled array of 100 distinct integers', function() {
-    var shuffle = require('../../lib/misc/shuffle.js');
-    var h = require('../../lib/sorting/helpers.js');
-
     var arr = [];
     for (var i = 1; i <= 100; i++) { arr.push(i); }
     var sh = shuffle(arr);
-
 
     context('setup', function() {
       it('confirms the array is NOT sorted', function() {
